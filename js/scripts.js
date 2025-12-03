7201e745600f2f75f4e28de3fc9dec17611ba395
officialmhrabbi <corabbi@gmail.com>
2025-12-03 10:55:51 +0600

    Add mobile menu toggle and jQuery smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 console.log("Hi! Welcome to my portfolio site.");
+
+$(function () {
+  $('a[href^="#"]').on("click", function (event) {
+    var target = $($(this).attr("href"));
+
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        500
+      );
+    }
+  });
+});
